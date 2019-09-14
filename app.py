46c30261c542ce2157514b9164b2a0a605af787c
George Aoyagi <aoyagigeorge@gmail.com>
2019-09-13 20:38:20 -0700

    got it to show gifs

--- app.py
+++ app.py
@@ -4,20 +4,29 @@ import json
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
-
+    query = request.args.get('q')
     # TODO: Make 'params' dict with query term and API key
-
+    params = {
+    "q": 'test',
+    "key": "U78L9P5SJIJZ",
+    "limit":10
+    }
     # TODO: Make an API call to Tenor using the 'requests' library
-
+    response = requests.get('https://api.tenor.com/v1/search', params=params) #adds params items to the end of the url      #response is an object misc response info
     # TODO: Get the first 10 results from the search results
-
+    response_json = response.json()       #converts response info into giant json dictionary
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    return render_template("index.html", results = response_json["results"])
 
-    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+#api key: U78L9P5SJIJZ 
