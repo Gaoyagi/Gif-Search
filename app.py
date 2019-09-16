a192d29af4410ad8ec720a22238d8dedf91cb42d
George Aoyagi <aoyagigeorge@gmail.com>
2019-09-16 14:49:30 -0700

    git search now works, no css tho

--- app.py
+++ app.py
@@ -13,16 +13,16 @@ def index():
     query = request.args.get('q')
     # TODO: Make 'params' dict with query term and API key
     params = {
-    "q": 'test',
-    "key": "U78L9P5SJIJZ",
-    "limit":10
+    "q": query,                     #tells tenor the keyword you want to search
+    "key": "U78L9P5SJIJZ",          #lets you access tenor with the api key
+    "limit":10                      #tells pc how many gifs u want
     }
     # TODO: Make an API call to Tenor using the 'requests' library
     response = requests.get('https://api.tenor.com/v1/search', params=params) #adds params items to the end of the url      #response is an object misc response info
     # TODO: Get the first 10 results from the search results
     response_json = response.json()       #converts response info into giant json dictionary
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
-    return render_template("index.html", results = response_json["results"])
+    return render_template("index.html", results = response_json["results"])               #links the index.html file to this function and passes the json dic into index html
 
 
 if __name__ == '__main__':
